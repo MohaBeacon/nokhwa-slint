a07782223abe3661004e537b6e3139b546eb7ec5
Mohan <mohanvjm@outlook.com>
2023-04-18 19:46:15 +0530

    test success

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use nokhwa::{
 
 
 use slint::{Timer, TimerMode, Image};
-const CANVAS_WIDTH: u32 = 1280;
-const CANVAS_HEIGHT: u32 = 720;
-const FPS:f32 = 30.0;
+const CANVAS_WIDTH: u32 = 640;
+const CANVAS_HEIGHT: u32 = 480;
+const FPS:f32 = 80.0;
 
 
 use slint::slint;
@@ -21,8 +21,8 @@ slint!{
 export component Main inherits Window {
     title: "slint";
     icon: @image-url("");
-    width: 1290px;
-    height: 730px;
+    width: 810px;
+    height: 650px;
 
     pure callback render-image(int) -> image;
     in-out property <int> frame;
@@ -33,8 +33,8 @@ export component Main inherits Window {
             Rectangle {
                 border-color: white;
                 border-width: 1px;
-                width: 1280px;
-                height: 720px;
+                width: 650px;
+                height: 490px;
                 Image {
                     width: 100%;
                     height: 100%;
@@ -56,7 +56,7 @@ fn main() -> Result<()>{
     let (frame_sender, frame_receiver) = channel();
     let (exit_sender, exit_receiver) = channel();
 
-    let mut frame_data = vec![0; (CANVAS_WIDTH * CANVAS_HEIGHT * 16) as usize];
+    let mut frame_data = vec![0; (CANVAS_WIDTH * CANVAS_HEIGHT * 4) as usize];
 
     timer.start(TimerMode::Repeated, std::time::Duration::from_secs_f32(1./FPS), move || {
         if let Some(window) = window_clone.upgrade(){
@@ -100,7 +100,7 @@ fn start(frame_sender: Sender<Vec<u8>>, exit_receiver: Receiver<()>) -> JoinHand
     spawn(move || -> Result<()>{
         let mut camera = Camera::new(
             Index(0), // index
-            RequestedFormat::new::<RgbFormat>(RequestedFormatType::HighestResolution(Resolution::new(1280, 720)))
+            RequestedFormat::new::<RgbFormat>(RequestedFormatType::HighestResolution(Resolution::new(640, 480)))
         )
         .unwrap();
         camera.open_stream().unwrap();
@@ -112,6 +112,7 @@ fn start(frame_sender: Sender<Vec<u8>>, exit_receiver: Receiver<()>) -> JoinHand
             let frame_rgba = camera.frame().unwrap();            
             // Convert the image buffer to a dynamic image
             let pixels = frame_rgba.decode_image::<RgbAFormat>().unwrap();
+            
             
             frame_sender.send(pixels.to_vec())?;
 
